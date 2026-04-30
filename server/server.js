d2a8e6a3d8a7dc7d4361186f778b0d3cc4ff18bd
damdinjav <damia0917sharav@gmail.com>
2026-04-30 17:12:22 +0800

    Temporarily disable order routes

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const authRoutes = require("./src/routes/authRoutes");
 const productRoutes = require("./src/routes/productRoutes");
-const orderRoutes = require("./src/routes/orderRoutes");
+//const orderRoutes = require("./src/routes/orderRoutes");
 
 const app = express();
 
@@ -19,7 +19,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/orders", orderRoutes);
+//app.use("/api/orders", orderRoutes);
 
 app.get("/", (req, res) => {
   res.send("Generator Ordering System API is running...");
